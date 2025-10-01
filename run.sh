886c680f94b83a35d3e191e86c68b117a1824039
Emil Skogsund <emil@skogsund.se>
2025-10-01 19:24:26 +0200

    basic yay with aur_builder

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ansible-playbook $HOME/.ansible/playbooks/all.yml
+ansible-playbook --become-password-file /home/skogix/.ssh/ansible-become-password --vault-password-file /home/skogix/.ssh/ansible-vault-password playbooks/all.yml
