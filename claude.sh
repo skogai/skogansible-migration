a80748794805caf82da13e47caa319e4b03c4a70
Emil Skogsund <emil@skogsund.se>
2025-12-26 02:42:31 +0100

    skogix

--- claude.sh
+++ claude.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-claude --setting-sources "project,local" --settings /home/skogix/dev/marketplace/.claude/settings.json --no-chrome --plugin-dir /home/skogix/.ansible/.claude/skills/ansible-core /home/skogix/.ansible/.claude/skills/using-git-worktrees/
+claude --setting-sources "project,local" --settings /home/skogix/dev/marketplace/.claude/settings.json --no-chrome --plugin-dir /home/skogix/.ansible/.claude/skills/ansible-core /home/skogix/.ansible/.claude/skills/using-git-worktrees/ "$@"
