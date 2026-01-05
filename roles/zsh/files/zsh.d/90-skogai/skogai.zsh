772850e245e4079f3aa7393e6894c7f30ee77ded
Emil Skogsund <emil@skogsund.se>
2026-01-05 15:20:33 +0100

    WIP

--- roles/zsh/files/zsh.d/90-skogai/skogai.zsh
+++ roles/zsh/files/zsh.d/90-skogai/skogai.zsh
@@ -165,3 +165,25 @@ alias drmi='docker rmi'
 alias ip='curl -s ifconfig.me'
 alias localip='ipconfig getifaddr en0'
 alias ports='netstat -tulanp'
+
+# Loop through all files in the ~/.config/fabric/patterns directory
+for pattern_file in $HOME/.config/fabric/patterns/*; do
+    # Get the base name of the file (i.e., remove the directory path)
+    pattern_name=$(basename "$pattern_file")
+
+    # Create an alias in the form: alias pattern_name="fabric --pattern pattern_name"
+    alias_command="alias $pattern_name='fabric-ai --pattern $pattern_name'"
+
+    # Evaluate the alias command to add it to the current shell
+    eval "$alias_command"
+done
+
+yt() {
+    local video_link="$1"
+    fabric-ai -y "$video_link" --transcript --dry-run
+}
+
+autoload -Uz compinit
+if ! type _compinit &>/dev/null; then
+  compinit
+fi
