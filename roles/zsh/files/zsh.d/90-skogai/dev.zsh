9cce88ed2bf57a35c29ed02608912fa0fd160285
Emil Skogsund <emil@skogsund.se>
2026-01-09 06:57:16 +0100

    (no commit message provided)

--- roles/zsh/files/zsh.d/90-skogai/dev.zsh
+++ roles/zsh/files/zsh.d/90-skogai/dev.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-alias commit='aider --commit --model openrouter/meta-llama/llama-3-70b-instruct:nitro --no-gitignore --set-env OPENROUTER_API_KEY=$OPENROUTER_API_KEY'
+alias commit='aider --commit --model openrouter/meta-llama/codellama-34b-instruct --no-gitignore --set-env OPENROUTER_API_KEY=$OPENROUTER_API_KEY'
 # alias gemini='node /home/skogix/.local/src/gemini-cli/scripts/start.js'
 if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
 alias wts='wt beta select'
