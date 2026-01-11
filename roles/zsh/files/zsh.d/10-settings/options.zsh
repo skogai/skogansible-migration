3afa39b227b7f81d2d38020552e95bc95d8784c5
Emil Skogsund <emil@skogsund.se>
2026-01-11 03:08:38 +0100

    .

--- roles/zsh/files/zsh.d/10-settings/options.zsh
+++ roles/zsh/files/zsh.d/10-settings/options.zsh
@@ -3,14 +3,15 @@
 # Configures shell behavior and features
 
 # Directory navigation
-# setopt AUTO_CD                   # cd to directory by typing its name
-# setopt AUTO_PUSHD                # Push directories onto stack automatically
-# setopt PUSHD_IGNORE_DUPS         # Don't push duplicates onto directory stack
-# setopt PUSHD_SILENT              # Don't print directory stack
-#
-# # Shell behavior
-# setopt INTERACTIVE_COMMENTS      # Allow comments in interactive shell
-# setopt PROMPT_SUBST              # Enable parameter expansion in prompts
-#
-# # Disable unwanted features
-# unsetopt CORRECT                 # Don't auto-correct commands (annoying)
+setopt AUTO_CD                   # cd to directory by typing its name
+setopt AUTO_PUSHD                # Push directories onto stack automatically
+setopt PUSHD_IGNORE_DUPS         # Don't push duplicates onto directory stack
+setopt PUSHD_SILENT              # Don't print directory stack
+
+# Shell behavior
+setopt INTERACTIVE_COMMENTS      # Allow comments in interactive shell
+setopt PROMPT_SUBST              # Enable parameter expansion in prompts
+
+# Disable unwanted features
+unsetopt CORRECT                 # Don't auto-correct commands
+unsetopt CORRECT_ALL             # Don't auto-correct command arguments
