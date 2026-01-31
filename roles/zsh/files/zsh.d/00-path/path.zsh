1ae804c7fc0fa8d205b99ad286b2ff351703b50c
Emil Skogsund <emil@skogsund.se>
2026-01-31 22:02:05 +0100

    WIPm

---
+++ roles/zsh/files/zsh.d/00-path/path.zsh
@@ -0,0 +1,5 @@
+export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$GOPATH/bin:$PATH"
+export PATH="$HOME/.local/share/pnpm:$PATH"
+export PATH="$HOME/.opencode/bin:$PATH"
---
+++ roles/zsh/files/zsh.d/30-aliases/build.zsh
@@ -0,0 +1,3 @@
+alias make='make -j$(nproc)'
+alias ninja='ninja -j$(nproc)'
+alias n='ninja'
