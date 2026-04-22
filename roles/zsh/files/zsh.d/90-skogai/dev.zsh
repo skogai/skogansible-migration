1cb86695b57b9c6b91099c278398efd97909944d
Emil Skogsund <emil@skogsund.se>
2026-04-22 08:45:37 +0200

    refactor(zsh): Change wt select alias to wt switch

--- roles/zsh/files/zsh.d/90-skogai/dev.zsh
+++ roles/zsh/files/zsh.d/90-skogai/dev.zsh
@@ -4,4 +4,4 @@ alias commitadd='wt step commit --stage tracked'
 alias commitadd='wt step commit --stage none'
 # alias gemini='node /home/skogix/.local/src/gemini-cli/scripts/start.js'
 if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
-alias wts='wt select'
+alias wts='wt switch'
