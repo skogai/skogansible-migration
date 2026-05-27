14baca5768d9606c85ffd874f3b5d1d5668a25cb
Emil Skogsund <emil@skogsund.se>
2026-05-27 15:09:55 +0200

    skogix; rootless docker and always venv

--- run.sh
+++ run.sh
@@ -27,6 +27,9 @@ check_password_file_permissions "$ANSIBLE_BECOME_PASSWORD_FILE"
 check_password_file_permissions "$ANSIBLE_VAULT_PASSWORD_FILE"
 
 # Activate venv if not already active
+uv venv --seed --clear
+uv pip install ansible-core
+uv pip install ansible
 if [ -z "$VIRTUAL_ENV" ] && [ -d ".venv" ]; then
   source .venv/bin/activate
 fi
